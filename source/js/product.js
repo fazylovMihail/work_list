b1cb7a207f8549cc3683ed12a9527a82f01f159a
fazylovMihail <fazylovmihail726@gmail.com>
2025-05-31 17:48:27 +0500

    Изменение архитрекуры проекта

--- source/js/product.js
+++
@@ -1,6 +0,0 @@
-import { delete_cookie } from "./cookie.js";
-
-document.getElementById('sign_out_btn').addEventListener('click', () => {
-    delete_cookie('login');
-    window.location.href = '../../index.html';
-});
