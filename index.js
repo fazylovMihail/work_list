aea120d11f1048cdb50681efb8064436e1350493
fazylovMihail <fazylovmihail726@gmail.com>
2025-06-15 13:51:48 +0500

    Рыбота над оптимизацией
    Была оптимизирована в ф-ю работа над сменой кнопок, а также полноценно работает смена кнопок

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let click = false;
 const wrapper = document.querySelector('.product_wrapper'), [left_btn, right_btn] = document.querySelectorAll('.header_btn_wrapper button');
 
 left_btn.onclick = () => add(wrapper);
-right_btn.onclick = () => click = highlight();
+right_btn.onclick = () => click = highlight(wrapper, click);
--- source/js/header_btns_event.js
+++ source/js/header_btns_event.js
@@ -7,21 +7,33 @@ export function add(wrapper){
     const product = new Product(text); product_arr.push(product); wrapper.appendChild(product.Drow_Card());
 
     const switch_btns_wrappers = wrapper.querySelectorAll('.switch_btns_wrapper'), switch_btns_wrapper = switch_btns_wrappers[switch_btns_wrappers.length-1];
-    switch_btns_wrapper.innerHTML = product.Drow_Base_Btns();
-
-    const delete_btns = wrapper.querySelectorAll('.delete'); delete_btns.forEach(delete_btn => delete_btn.onclick = () => wrapper.removeChild(delete_btn.parentNode.parentNode));
+    click_manager(switch_btns_wrapper, wrapper, true, product_arr.length-1);
 }
-export function highlight(){
+export function highlight(wrapper, click){
     if(product_arr.length < 2){alert('Карточек должно быть больше одной, чтобы применить выделение :)'); return}
 
     for(let i=0;i<product_arr.length;i++){
         const card = product_arr[i].card; console.log(card);
-        const switch_btns_wrapper = card.querySelector('.switch_btns_wrapper'); switch_btns_wrapper.innerHTML = product_arr[i].Drow_Highlight_Btns();
+        const switch_btns_wrapper = card.querySelector('.switch_btns_wrapper');
 
-        const highlight_btn = switch_btns_wrapper.querySelector('.highlight'); highlight_btn.onclick = () => product_arr[i].highlight_click = highlight_click_manager(highlight_btn, product_arr[i].highlight_click);
+        click_manager(switch_btns_wrapper, wrapper, click, i);
     }
+    return click = !click;
 }
 
+function click_manager(switch_btns_wrapper, wrapper, click, index){
+    if(click){
+        switch_btns_wrapper.innerHTML = product_arr[index].Drow_Base_Btns();
+        const delete_btn = wrapper.querySelectorAll('.delete')[index];
+        delete_btn.onclick = () => {
+            wrapper.removeChild(delete_btn.parentNode.parentNode);
+            product_arr.splice(product_arr[index], 1); console.log(product_arr);
+        }        
+    } else{
+        switch_btns_wrapper.innerHTML = product_arr[index].Drow_Highlight_Btns();
+        const highlight_btn = switch_btns_wrapper.querySelector('.highlight'); highlight_btn.onclick = () => product_arr[index].highlight_click = highlight_click_manager(highlight_btn, product_arr[index].highlight_click);
+    }
+}
 function highlight_click_manager(btn, highlight_click){
     const img = btn.querySelector('img');
     highlight_click? img.style.display = 'none': img.style.display = 'block'; highlight_click = !highlight_click;
